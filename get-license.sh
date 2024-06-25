92b3ed71c1152f6846859db6a7d804359e1ea9ec
Ruslan <106345026+ackn0wl3dgm3nt@users.noreply.github.com>
2024-06-25 11:51:23 +0300

    Create get-license.sh

---
+++ get-license.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pip3 install hwid
+python3 -c "import hwid;import hashlib;print('HWID: '+hwid.get_hwid());print('SHA256: '+hashlib.sha256(hwid.get_hwid().encode()).hexdigest())"
+pip3 uninstall hwid
