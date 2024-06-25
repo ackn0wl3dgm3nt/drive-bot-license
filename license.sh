58e6eb6450654b0afdcb4af48c0fb2674b48f0b1
Ruslan <106345026+ackn0wl3dgm3nt@users.noreply.github.com>
2024-06-25 13:52:52 +0300

    Update license.sh

--- license.sh
+++ license.sh
@@ -4,5 +4,5 @@
 	sudo apt install python3-pip
 	pip3 install hwid
 	python3 -c "import hwid;import hashlib;print('HWID: '+hwid.get_hwid());print('SHA256: '+hashlib.sha256(hwid.get_hwid().encode()).hexdigest())"
-	pip3 uninstall hwid
+	pip3 uninstall hwid -y
 }
