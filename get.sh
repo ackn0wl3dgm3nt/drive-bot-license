a3b7635499993081ade44959cea8e97fc8b218c1
Ruslan <106345026+ackn0wl3dgm3nt@users.noreply.github.com>
2024-06-29 23:12:32 +0300

    Update get.sh

--- get.sh
+++ get.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-sudo dmidecode -s system-uuid | python3 -c "import sys, hashlib; uuid = sys.stdin.read().strip(); print(uuid); print(hashlib.sha256(uuid.encode()).hexdigest())"
+sudo dmidecode -s system-uuid | python3 -c "import sys, hashlib; uuid = sys.stdin.read().strip(); print('HWID: ' + uuid); print('SHA256: ' + hashlib.sha256(uuid.encode()).hexdigest())"
