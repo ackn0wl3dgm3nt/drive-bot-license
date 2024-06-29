d4b98fbef5d167a926f08f2e16c4b63ec54b8ac8
Ruslan <106345026+ackn0wl3dgm3nt@users.noreply.github.com>
2024-06-29 22:29:10 +0300

    Update get.sh

--- get.sh
+++ get.sh
@@ -4,5 +4,5 @@
 	sudo apt install python3-pip -y > /dev/null 2>&1
 	pip3 install hwid --break-system-packages > /dev/null 2>&1
 	python3 -c "import hwid; import hashlib; print('HWID: ' + hwid.get_hwid()); print('SHA256: ' + hashlib.sha256(hwid.get_hwid().encode()).hexdigest())"
-	pip3 uninstall hwid --break-system-packages > /dev/null 2>&1
+	pip3 uninstall hwid -y --break-system-packages > /dev/null 2>&1
 }
