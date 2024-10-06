020942249f93fee8a8afa107297aafad84ad3fb3
bash <106345026+ackn0wl3dgm3nt@users.noreply.github.com>
2024-10-07 01:23:15 +0300

    Update get.sh

--- get.sh
+++ get.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sudo apt install dmidecode -y
 sudo dmidecode -s system-uuid | python3 -c "import sys, hashlib; uuid = sys.stdin.read().strip(); print('HWID: ' + uuid); print('SHA256: ' + hashlib.sha256(uuid.encode()).hexdigest())"
